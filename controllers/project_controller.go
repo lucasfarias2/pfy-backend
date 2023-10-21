8b407c56eebe70ad97035dbe1a9729b88a4d67c2
Lucas Farias <lukf95@gmail.com>
2023-10-21 19:26:22 +0200

    Refactor

--- controllers/project_controller.go
+++ controllers/project_controller.go
@@ -5,6 +5,7 @@ import (
 	"packlify-cloud-backend/models"
 	"packlify-cloud-backend/models/constants"
 	"packlify-cloud-backend/services"
+	"time"
 )
 
 func CreateProject(c *fiber.Ctx) error {
@@ -14,10 +15,13 @@ func CreateProject(c *fiber.Ctx) error {
 	}
 
 	newProject, err := services.CreateProject(*project)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	}
 
 	tm := services.NewTaskManager()
 	createCloudBuildDone := make(chan bool)
-	//createCloudRunDone := make(chan bool)
+	createCloudRunDone := make(chan bool)
 	errs := make(chan error)
 
 	go func() {
@@ -27,6 +31,8 @@ func CreateProject(c *fiber.Ctx) error {
 			errs <- err
 			return
 		}
+
+		time.Sleep(20 * time.Second) // Faking the time it takes to complete the task
 
 		// err = services.CreateCloudBuild(newProject)
 		if err != nil {
@@ -45,9 +51,27 @@ func CreateProject(c *fiber.Ctx) error {
 		createCloudBuildDone <- true
 	}()
 
-	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
-	}
+	go func() {
+		<-createCloudBuildDone // wait for deployment to finish
+
+		// Create a new task for integration
+		task, err := tm.CreateTask(newProject.ID, constants.Running, "Creating Cloud Run", 5)
+		if err != nil {
+			errs <- err
+			return
+		}
+
+		//err = services.CreateCloudRun(newProject)
+		if err != nil {
+			err := tm.UpdateTaskStatus(task.ID, "Failed", err.Error())
+			errs <- err
+			return
+		}
+
+		// Update task status to Success
+		err = tm.UpdateTaskStatus(task.ID, constants.Success, "Cloud Run was created successfully")
+		createCloudRunDone <- true
+	}()
 
 	return c.JSON(newProject)
 }
